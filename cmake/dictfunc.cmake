6a70afa30eacd94c89b88965f0349d8a81d38bc3
Ezra Peisach <ezra.peisach@rcsb.org>
2020-11-13 22:05:47 -0500

    Add ability to create sdb files with dependencies

--- cmake/dictfunc.cmake
+++ cmake/dictfunc.cmake
@@ -3,7 +3,7 @@ function(BUILD_DICT DICTNAME)
   set(GEN_COMMAND
     ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/gendict.py
     -s ${CMAKE_CURRENT_SOURCE_DIR}/dicts/dict-${DICTNAME}
-    -o ${CMAKE_CURRENT_BINARY_DIR}/mmcif/${DICTNAME}
+    -o ${CMAKE_CURRENT_BINARY_DIR}/dicts/dict-${DICTNAME}
     --dict-name ${DICTNAME}
     )
 
@@ -39,9 +39,43 @@ function(BUILD_DICT DICTNAME)
     COMMENT "Generating the dictionary files for ${DICTNAME}."
     )
 
-  add_custom_target(${DICTNAME} ALL
+  add_custom_target(${DICTNAME}_dic ALL
     DEPENDS ${GEN_OUTPUTS}
     )
   # Remove explicitly directory
-  file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/mmcif/${DICTNAME})
+  file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/dicts/dict-${DICTNAME})
 endfunction()
+
+
+function(BUILD_SDB DICTNAME)
+  set(GEN_COMMAND
+    ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/gendict.py
+    -s ${CMAKE_CURRENT_SOURCE_DIR}/dicts/dict-${DICTNAME}
+    -o ${CMAKE_CURRENT_BINARY_DIR}/dicts/dict-${DICTNAME}
+    --dict-name ${DICTNAME}
+    )
+
+  execute_process(
+    COMMAND ${GEN_COMMAND} --print-outputs
+    OUTPUT_VARIABLE GEN_OUTPUTS
+    RESULT_VARIABLE RET
+    )
+
+  #message(STATUS "Outputs is " ${GEN_OUTPUTS})
+
+  if (NOT RET EQUAL 0)
+    message(FATAL_ERROR "Failed to get the GEN outputs" ${RET})
+  endif()
+
+  add_custom_target(${DICTNAME}_sdb ALL
+    ./bin/CreateDictSdbFile.csh ${DICTNAME}
+    DEPENDS ${DICTNAME}_dic  "DictToSdb" "sdb" "mmcif_ddl_dic"
+    COMMENT "Building SDB file for ${DICTNAME}"
+    )
+
+endfunction()
+
+# Command to ensure sdb directory exists
+add_custom_target("sdb" ALL
+  COMMAND ${CMAKE_COMMAND} -E make_directory sdb)
+
